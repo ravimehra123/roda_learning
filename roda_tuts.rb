8bf6e343e42610d005717761bc0a47ae3652ffac
Pankhuri Pravin <pankhuri.pratichi@gmail.com>
2016-07-28 16:03:29 +0530

    Second Commit

--- roda_tuts.rb
+++ roda_tuts.rb
@@ -1,9 +1,24 @@
 require "roda"
 
 class RodaTuts < Roda
+	plugin :static, ["/images", "/css", "/js"]
+  plugin :render
+  plugin :head
+
   route do |r|
+
     r.root do
-      "THIS IS LANDING PAGE FOR RODA AND SEQUEL!"
+      view("homepage")
     end
+
+    r.get "about" do
+	    view("about")
+	  end
+
+	  r.get "contact" do
+	    view("contact")
+	  end
+
   end
 end
+
